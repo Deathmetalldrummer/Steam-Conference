a3dfa9b06c60dd68c52b7947164ae69feb2e512f
Ragnarok <DeathMetallDrummer@gmail.com>
2020-02-06 15:40:44 +0600

    Added theme controller

--- source/devel/_layout/_mounting.js
+++ source/devel/_layout/_mounting.js
@@ -1,2 +1,4 @@
 //#include("./_header/_header.js");
 //#include("./_footer/_footer.js");
+
+//#include("./_theme_control/_theme_control.js");
---
+++ source/devel/_layout/_theme_control/_theme_control.js
@@ -0,0 +1,9 @@
+$('.themes__').on('click', function(e) {
+	var target = $(e.target);
+	var indexTheme = target.data('theme');
+	console.log($('body').attr('data-theme'));
+	$('body')
+		.removeClass('theme_' + $('body').attr('data-theme'))
+		.attr('data-theme', indexTheme)
+		.addClass('theme_' + indexTheme);
+})
