7e77fe6ff0adc1c28bbafe755b6572c9e255109b
Ragnarok <DeathMetallDrummer@gmail.com>
2020-02-12 14:44:22 +0600

    Added logo & social

--- source/devel/_layout/_mounting.js
+++ source/devel/_layout/_mounting.js
@@ -4,3 +4,6 @@
 //#include("./_theme_control/_theme_control.js");
 //#include("./_main_menu/_main_menu.js");
 //#include("./_decorative_horizontal_line/_decorative_horizontal_line.js");
+
+//#include("./_logo/_logo.js");
+//#include("./_social/_social.js");
