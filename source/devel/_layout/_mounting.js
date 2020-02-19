cf7c69bacf0a0ed3185eaae79f73eba8778351b1
Ragnarok <DeathMetallDrummer@gmail.com>
2020-02-20 00:25:55 +0600

    Added Dropdown

--- source/devel/_layout/_mounting.js
+++ source/devel/_layout/_mounting.js
@@ -2,8 +2,11 @@
 //#include("./_footer/_footer.js");
 
 //#include("./_theme_control/_theme_control.js");
+//#include("./_dropdown/_dropdown.js");
 //#include("./_main_menu/_main_menu.js");
 //#include("./_decorative_horizontal_line/_decorative_horizontal_line.js");
 
 //#include("./_logo/_logo.js");
 //#include("./_social/_social.js");
+//#include("./_main_submenu/_main_submenu.js");
+//#include("./_main_submenu/_submenuItem_category/_submenuItem_category.js");
